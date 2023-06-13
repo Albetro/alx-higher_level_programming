fe76a744231c1fa4876cd261b82153405dfd6254
Albetro <albertxang@gmail.com>
2023-06-13 05:13:12 +0300

    only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_l = []
-    for l in my_list:
-        if l % 2 == 0:
+    for q in my_list:
+        if q % 2 == 0:
             new_l.append(True)
         else:
             new_l.append(False)
