95f5c4de578cc59fb2b3926def748655a7e2464b
Albetro <albertxang@gmail.com>
2023-08-17 04:46:25 +0300

    cannot be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- this script that creates the table on MYSQL server
+-- if the table already exists, script should not fail
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT DEFAULT 1,
+	name VARCHAR(256)NOT NULL);
