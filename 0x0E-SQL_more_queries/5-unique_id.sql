2a6195fe3145a10afd17387cb314dd2ba276489a
Albetro <albertxang@gmail.com>
2023-08-17 04:58:50 +0300

    unique id

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+--this script that creates the table unique_id on my sql server
+-- if the table exists, script should not fail
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
